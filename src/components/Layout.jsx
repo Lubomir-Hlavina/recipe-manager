cdda81c6cf266034a901c8bb77f5f310f6b5e546
Lubomir-Hlavina <lubo.hlavina@gmail.com>
2025-04-27 10:33:43 +0200

    Layout component updated

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,17 +5,17 @@ const Layout = ({ children, onAddClick }) => {
   return (
     <>
       <AppBar position="static" elevation={0}>
-        <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+        <Toolbar data-cy="app-toolbar">
+          <Typography variant="h6" sx={{ flexGrow: 1 }} data-cy="toolbar-title">
             Recipe Manager
           </Typography>
-          <Button color="inherit" onClick={onAddClick}>
+          <Button color="inherit" onClick={onAddClick} data-cy="toolbar-add-btn">
             Add Recipe
           </Button>
         </Toolbar>
       </AppBar>
       <Box sx={{ p: 3 }}>
-        <Box sx={{ maxWidth: 600, mx: 'auto' }}>
+        <Box sx={{ maxWidth: 600, mx: 'auto' }} data-cy="layout-content">
           {children}
         </Box>
       </Box>
